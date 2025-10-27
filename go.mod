eb166d6ac3fbf912b8190eff465b8cdc1558e94a
Subham Chakraborty <subhamchakraborty81@gmail.com>
2025-10-27 06:31:26 +0000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module profit.calculator/m
+
+go 1.21.13
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue float64
+	var expenses float64
+	taxRate := 12.0
+
+	fmt.Print("Enter revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Enter expenses: ")
+	fmt.Scan(&expenses)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	fmt.Println(ebt)
+	fmt.Println(profit)
+	fmt.Println(ratio)
+}
